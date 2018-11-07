28733589bb666ad8aab6aa76bb852d89163e967b
ste001 <asso99@hotmail.com>
2018-11-07 22:14:05 +0100

    Solve 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add (a, b)
+    a+b
+end
+
+def subtract (a, b)
+    a-b
+end
+
+def sum (arr)
+    total = 0
+    arr.each do |i|
+        total += i.to_i
+    end
+    total
+end
+        
+def multiply (a, b)
+    a*b
+end
+
+def power (base, exp)
+    total = 1
+    while exp != 0
+        total *= base
+        exp = exp - 1
+    end
+    total
+end
+
+def factorial (num)
+    if num == 0 or num == 1
+        1
+    else 
+        num * factorial(num-1)
+    end
+end
