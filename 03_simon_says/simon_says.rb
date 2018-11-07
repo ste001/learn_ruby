6fda17ff73705d705c3f69c1009d1e2ac6ad0a34
ste001 <asso99@hotmail.com>
2018-11-07 22:37:25 +0100

    Solve start_of_word in 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
 #write your code here
+
+def echo (word)
+    word
+end
+
+def shout (word)
+    word.upcase
+end
+
+def repeat (word, n=2)
+    result = ""
+    (n-1).times do
+        result += word + " "
+    end
+    result += word
+    result
+end
+
+def start_of_word(word, start)
+    result = ""
+    i = 0
+    while (i != start)
+        result += word[i]
+        i = i+1
+    end
+    result
+end
