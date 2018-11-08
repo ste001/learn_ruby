a5c15bc4fc5b611f8e7f7f34992f3f9bf006413e
ste001 <asso99@hotmail.com>
2018-11-08 11:56:47 +0100

    Solve 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+
+def translate (sentence)
+    sentenceArr = sentence.split(" ")
+    s = sentenceArr.map do |i|
+        l = i.length
+        if /^[aeiou]/ .match(i)
+            i + "ay"
+         elsif /^qu/ .match(i[0..1])
+             i[2..(l+1)] + i[0..1] + "ay"
+         elsif /[aeiou]/ .match(i[1..1])
+             i[1..(l+1)] + i[0..0] + "ay"
+         else
+             i[2..(l+1)] + i[0..1] + "ay"
+        end
+    end
+    res = s.join(" ")        
+end
